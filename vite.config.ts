c30b555ce3394bc6e8f9c99c667d4a2b657fffa9
anthonychaussin <chaussin.anthony@gmail.com>
2025-07-17 02:09:58 +0200

    v 0.0.4 add esm bundle and update readme to use it

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
 import {defineConfig} from 'vite';
-import path from 'path';
 
 export default defineConfig({
-	                            build: {
-		                            lib: {
-			                            entry: path.resolve(__dirname, 'src/index.ts'),
-			                            name: 'NotebookViewerTs',
-			                            fileName: (format) => `notebook-viewer-ts.${format}.js`,
-			                            formats: ['es', 'umd'],
-		                            },
-		                            rollupOptions: {
-			                            output: {
-				                            globals: {
-					                            marked: 'marked',
-					                            'highlight.js': 'hljs',
-				                            },
-			                            },
-			                            external: [],
-		                            },
-		                            minify: true,
-		                            outDir: 'cdn',
-		                            emptyOutDir: true,
+	                            server: {
+		                            port: 3000,
 	                            }
                             });
